32bc6ea9d2c4f9cd532d300feda61c883687aeee
ciastka <ciastka@gmail.com>
2017-12-09 14:53:57 +0100

    first commit

---
+++ python_intro.py
@@ -0,0 +1,76 @@
+#
+# if  3 > 2:
+#     print ('It works!')
+#
+# if 6 > 4:
+#     print('6 is indeed greater than 4')
+# else:
+#     print('6 is not greater than 2')
+#
+# if 6 < 3:
+#     print('6 is not greater than 3')
+# else:
+#     print('6 is indeed greater than 3')
+#
+# # name = input('podaj imie: ')
+# # print(name)
+# # if name == 'Zosia':
+# #     print('Cześć Zosia')
+# # elif name == 'Lina':
+# #     print('Cześć Lina :)')
+# # else:
+# #     print('Cześć Anonimie')
+#
+# volume = 91
+# if volume < 20:
+#     print('Nic nie słychać')
+# elif 20 <= volume < 70:
+#     print('Fajnie, ale może glośniej')
+# elif 70 <= volume < 90:
+#     print('Idelanie!')
+# elif 90 <= volume < 100:
+#     print('Sąsiadom też się podoba ;)')
+# else:
+#     print('...')
+
+# 123
+
+# def hi():
+#     print('Hi!')
+#     print('How are you?')
+#
+# hi()
+#
+# def hi(name):
+#     if name == 'Zosia':
+#         print('Cześć Zosia')
+#     elif name == 'Ola':
+#         print('Cześć Ola!')
+#     else:
+#         print('Hej!')
+#
+# hi(input('podaj imię: '))
+
+
+# def hi(name):
+#     print('Cześć ' + name)
+#
+# hi(input('podaj imię: '))
+
+
+
+#
+# def hi(name):
+#     print('Hej ' + name + ' :)')
+#
+# girls = ['Ania', 'Ola', 'Zosia', 'Ty']
+# for name in girls:
+#     hi(name)
+#     print('Next girl')
+
+
+for i in range(1,24):
+    print(i)
+
+for i in range(1,6,2):
+    print(i)
